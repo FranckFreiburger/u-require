3809cba393447e3b4c6ee373fb6246832ce874d5
Franck Freiburger <franck.freiburger@gmail.com>
2017-05-22 23:58:36 +0200

    - make extensions customizable - fix relative path

--- src/uRequire.js
+++ src/uRequire.js
@@ -1,39 +1,51 @@
 // equivalent to require from node.js
 require.cache = {};
 
+require.httpRequest = function(url) {
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url, false); // sync
+	xhr.send();
+	if ( xhr.status < 200 || xhr.status >= 300 )
+		throw new Error( xhr.statusText );
+	return xhr.responseText;
+}
+
+require.extensions = {
+	'.js': function(module, url) {
+		
+		var responseText = require.httpRequest(url);
+
+		try {
+
+			Function('exports', 'require', 'module', responseText).call(module.exports, module.exports, module.require, module);
+		} catch (err) {
+			
+			throw new Error( 'Error loading module '+url+': '+err );
+		}
+	}
+}
+
 function require(url) {
 	
 	var filenamePos = url.lastIndexOf('/')+1;
 	var extDotPos = url.indexOf('.', filenamePos);
 
-	if ( extDotPos === -1 )
-		url += '.js';
+	var extension = extDotPos !== -1 ? url.substr(extDotPos) : '.js';
 	
 	var exports = require.cache[url];
 	
 	if ( !exports ) {
 		
-		try {
-			exports = {};
-			var xhr = new XMLHttpRequest();
-			xhr.open('GET', url, false); // sync
-			xhr.send();
-			if ( xhr.status < 200 || xhr.status >= 300 )
-				throw new Error( xhr.statusText );
-			
-			var module = { id: url, uri: url, exports: exports };
+		function childModuleRequire(childUrl) {
 
-			function childRequire(childUrl) {
-				
-				return require((childUrl.substr(0,2) === './' ? url.substr(0, filenamePos) : '') + childUrl);
-			}
-
-			Function('exports', 'require', 'module', xhr.responseText).call(module.exports, module.exports, childRequire, module);
-			require.cache[url] = exports = module.exports;
-		} catch (err) {
-			
-			throw new Error( 'Error loading module '+url+': '+err );
+			return require((childUrl.substr(0,2) === './' || childUrl.substr(0,3) === '../' ? url.substr(0, filenamePos) : '') + childUrl);
 		}
+
+		exports = {};
+		var module = { id: url, uri: url, exports: exports, require:childModuleRequire };
+		require.extensions[extension](module, url);
+		require.cache[url] = exports = module.exports;
 	}
 	
 	return exports;
